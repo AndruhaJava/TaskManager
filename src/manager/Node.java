8eaf178508319d320c5feabfd867c882b482e1cd
Andruha <andrejeriomin@yandex.ru>
2024-10-30 22:35:23 +0300

    Completed Task retry again

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -1,22 +1,15 @@
 package manager;
 
-class Node <Task> {
 
-    private Task data;
+class Node<Task> {
+
+    public Task data;
     public Node<Task> next;
     public Node<Task> previous;
 
     public Node(Node<Task> previous, Task data, Node<Task> next) {
-        this.setData(data);
+        this.data = data;
         this.next = next;
         this.previous = previous;
-    }
-
-    public Task getData() {
-        return data;
-    }
-
-    public void setData(Task data) {
-        this.data = data;
     }
 }
